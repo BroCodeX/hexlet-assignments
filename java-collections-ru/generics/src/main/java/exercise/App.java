34c62cdb05d5909ef9635a7204cc94966cd1f1cf
@hexlet/cli <support@hexlet.io>
2024-02-16 11:39:22 +0300

    submit java-collections-ru/generics

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -8,20 +8,20 @@ import java.util.ArrayList;
 class App {
     public static List<Map<String, String>> findWhere(List<Map<String, String>> books, Map<String, String> where) {
         List<Map<String, String>> filteredBooks = new ArrayList<>();
+        MatchesCount counter = ((map, key, value) -> map.get(key).equals(value) ? 1 : 0);
         books.forEach(x -> {
             int check = 0;
             for (Map.Entry<String, String> kv : where.entrySet()) {
-                check += matchesCount(x, kv.getKey(), kv.getValue());
+                check += counter.apply(x, kv.getKey(), kv.getValue());
             }
             if (check == where.size()) filteredBooks.add(x);
         });
         return filteredBooks;
     }
+}
 
-    public static int matchesCount(Map<String, String> x, String whereKey, String whereValue) {
-        return x.get(whereKey).equals(whereValue) ? 1 : 0;
-    }
-
-
+@FunctionalInterface
+interface MatchesCount {
+    int apply (Map<String, String> x, String whereKey, String whereValue);
 }
 //END
