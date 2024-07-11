3f70b875680d9d27e3f6fea28aa27bf5e27872c9
@hexlet/cli <support@hexlet.io>
2024-07-11 07:57:08 +0300

    submit java-web-ru/session

---
+++ java-web-ru/session/src/main/java/exercise/controller/SessionsController.java
@@ -0,0 +1,47 @@
+package exercise.controller;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
+import exercise.dto.MainPage;
+import exercise.dto.LoginPage;
+import exercise.repository.UsersRepository;
+import static exercise.util.Security.encrypt;
+
+import exercise.util.Generator;
+import exercise.util.NamedRoutes;
+import exercise.util.Security;
+import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
+
+public class SessionsController {
+
+    // BEGIN
+    public static void build (Context context) {
+        LoginPage page = new LoginPage(context.sessionAttribute("auth"), null);
+        context.render("build.jte", model("page", page));
+    }
+
+    public static void index (Context context) {
+        LoginPage page = new LoginPage(context.sessionAttribute("auth"), null);
+        context.render("index.jte", model("page", page));
+    }
+
+    public static void create (Context context) {
+        String name = context.formParam("name");
+        String password = context.formParam("password");
+        String encryptPassword = Security.encrypt(password);
+        var user = UsersRepository.findByName(name);
+        if (user == null || !user.getPassword().equals(encryptPassword)) {
+            LoginPage page = new LoginPage(name, "Wrong username or password");
+            context.render("build.jte", model("page", page)).status(302);
+        } else {
+            context.sessionAttribute("auth", name);
+            context.redirect(NamedRoutes.rootPath());
+        }
+    }
+
+    public static void destroy (Context context) {
+        context.sessionAttribute("auth", null);
+        context.redirect(NamedRoutes.rootPath());
+    }
+    // END
+}
