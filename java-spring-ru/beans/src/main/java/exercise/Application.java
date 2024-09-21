6e69765e06fedb559c1fc024c84aacab9e615ff7
@hexlet/cli <support@hexlet.io>
2024-09-21 09:35:38 +0300

    submit java-spring-ru/beans

--- java-spring-ru/beans/src/main/java/exercise/Application.java
+++ java-spring-ru/beans/src/main/java/exercise/Application.java
@@ -8,10 +8,11 @@ import java.time.LocalDateTime;
 import exercise.daytime.Daytime;
 import exercise.daytime.Day;
 import exercise.daytime.Night;
-import org.springframework.context.annotation.Bean;
+
 
 // BEGIN
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 // END
 
 @SpringBootApplication
@@ -23,6 +24,7 @@ public class Application {
 
     // BEGIN
     @Bean
+    @Scope("prototype")
     public Daytime getShowTime() {
         int curHour = LocalDateTime.now().getHour();
         if (curHour >= 6 && curHour < 22) {
--- java-spring-ru/beans/src/main/java/exercise/daytime/Daytime.java
+++ java-spring-ru/beans/src/main/java/exercise/daytime/Daytime.java
@@ -3,11 +3,6 @@ package exercise.daytime;
 // Интерфейс содержит метод, возвращающий название времени суток
 // Реализация методов представлена в классах Morning, Day, Evening, Night,
 // которые реализуют этот интерфейс
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-
-@Component
-@Scope("prototype")
 public interface Daytime {
 
     String getName();
