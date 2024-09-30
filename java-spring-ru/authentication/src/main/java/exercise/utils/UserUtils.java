be1758bd74f4129871a8bec7dd082d6a5851c4c7
@hexlet/cli <support@hexlet.io>
2024-09-30 15:38:26 +0300

    submit java-spring-ru/authentication

---
+++ java-spring-ru/authentication/src/main/java/exercise/utils/UserUtils.java
@@ -0,0 +1,30 @@
+package exercise.utils;
+
+import exercise.model.User;
+import exercise.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserUtils {
+    @Autowired
+    private UserRepository userRepository;
+
+    // BEGIN
+    public User getCurrentUser() {
+        var authentification = SecurityContextHolder.getContext().getAuthentication();
+        if (authentification == null || !authentification.isAuthenticated()) {
+            return null;
+        }
+        var name = authentification.getName();
+        return userRepository.findByEmail(name)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+    // END
+
+    public User getTestUser() {
+        return  userRepository.findByEmail("hexlet@example.com")
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+}
