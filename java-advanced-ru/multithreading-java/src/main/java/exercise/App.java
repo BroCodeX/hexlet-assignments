ba7395bd837b76802643d9944b85646f6f96b22e
@hexlet/cli <support@hexlet.io>
2024-10-17 09:06:21 +0300

    submit java-advanced-ru/multithreading-java

---
+++ java-advanced-ru/multithreading-java/src/main/java/exercise/App.java
@@ -0,0 +1,39 @@
+package exercise;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+class App {
+    private static final Logger LOGGER = Logger.getLogger("AppLogger");
+
+    // BEGIN
+    public static void main(String[] args) {
+        int[] numbers = {10, -4, 67, 100, -100, 8};
+        Map<String, Integer> result = getMinMax(numbers);
+        System.out.println(result);
+    }
+
+    public static Map<String, Integer> getMinMax(int[] numbers) {
+        Map<String, Integer> result = new HashMap<>();
+
+        MinThread minThread = new MinThread(numbers);
+        MaxThread maxThread = new MaxThread(numbers);
+
+        maxThread.start();
+        minThread.start();
+
+        try {
+            minThread.join();
+            maxThread.join();
+        } catch (InterruptedException e) {
+            LOGGER.info("Thread interrupted: " + e.getMessage());
+        }
+
+        result.put("min", minThread.getResult());
+        result.put("max", maxThread.getResult());
+
+        return result;
+    }
+    // END
+}
---
+++ java-advanced-ru/multithreading-java/src/main/java/exercise/MinThread.java
@@ -0,0 +1,27 @@
+package exercise;
+
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+// BEGIN
+public class MinThread extends Thread{
+    private Integer result;
+    private int[] numbers;
+    private static final Logger LOGGER = Logger.getLogger("AppLogger");
+
+    public MinThread(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    @Override
+    public void run() {
+        LOGGER.info("Thread " + Thread.currentThread().getName() + " started");
+        result = Arrays.stream(numbers).min().getAsInt();
+        LOGGER.info("Thread " + Thread.currentThread().getName() + " finished");
+    }
+
+    public Integer getResult() {
+        return result;
+    }
+}
+// END
